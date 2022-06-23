e62192990426b455188a7a4040661dca095f0705
herrmeyerL <logern5@gmail.com>
2022-06-23 15:48:42 -0600

    Add a cool graph

--- pages/work_page.py
+++ pages/work_page.py
@@ -1,6 +1,8 @@
 # Run main_page.py first
 import streamlit as st
 import pandas as pd
+from matplotlib import pyplot as plt
+from plotnine import *
 
 # Set the page name
 st.sidebar.markdown("# Work Page")
@@ -19,7 +21,7 @@ st.write("## Our dataset:")
 st.write(df)
 
 # Show a table of the top movies by budget, with a slider to limit the number shown.
-st.write(f"## Top highest budgeted movies:")
+st.write("## Top highest budgeted movies:")
 x = st.slider('Number of movies to display:', value=5, min_value=1, max_value=50)
 # Use a cache so we can load display our sorted data quickly
 @st.cache
@@ -27,3 +29,13 @@ def get_data_sorted_by_budget():
     return df.sort_values("budget",ascending=False) 
 df_sorted_by_budget = get_data_sorted_by_budget()
 st.write(df_sorted_by_budget.head(x))
+
+# Show a scatter plot of movie budget vs year
+st.write("## Movie budget vs. year")
+plot = (
+    ggplot(df.sample(1000, random_state=42).query('budget > 100'), aes(x='year',y='budget'))
+    + geom_point()
+    + geom_smooth()
+    + theme_bw()
+).draw(show=False)
+st.write(plot)
