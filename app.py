86d5957abd232e1094a7e85199b443f4ef1c9261
herrmeyerL <logern5@gmail.com>
2022-06-23 15:10:47 -0600

    Comment stuff

--- app.py
+++ app.py
@@ -2,14 +2,17 @@
 import streamlit as st
 import pandas as pd
 
-# Load in data
+# Load in data.
 df = pd.read_csv("data.csv").drop(['_c0'],axis=1)
 
+# Write the title header.
 st.write("""# Muskrats Streamlit App""")
 
+# Show a table of the entire dataset.
 st.write("## Our dataset:")
 st.write(df)
 
+# Show a table of the top movies by budget, with a slider to limit the number shown.
 st.write("## Top highest budgeted movies:")
 x = st.slider('Number of movies to display:', value=5, min_value=1, max_value=50)
 st.write(df.sort_values("budget",ascending=False).head(x))
